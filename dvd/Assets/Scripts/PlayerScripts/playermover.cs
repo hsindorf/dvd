4154e8a120f8a53e952d2875902599bfc05f806a
turnipsoup <jeremy.heckt@gmail.com>
2019-05-25 17:24:04 -0700

    movement test...not flipping right yet

---
+++ dvd/Assets/Scripts/PlayerScripts/playermover.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void Flip(){
+	    // Switch the way the player is labelled as facing
+	    // Multiply the player's x local scale by -1
+	    Vector3 theScale = transform.localScale;
+	    theScale.x *= -1;
+	    transform.localScale = theScale;
+	}
+
+    // Public Variables
+    public float speed = 1.2f;
+    public bool facingRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (Input.GetKey(KeyCode.D)) {
+    		Flip();
+    		facingRight = true;
+
+    		Vector3 pos = transform.position;
+    		pos.x += speed * Time.deltaTime;
+    		transform.position = pos;
+    	}
+
+
+
+
+    	if (Input.GetKey(KeyCode.A)) {
+    		Flip();
+    		facingRight = false;
+
+    		Vector3 pos = transform.position;
+    		pos.x -= speed * Time.deltaTime;
+    		transform.position = pos;
+    	}
+        
+    }
+}
+
+
+/* 
+Here is the stuff you need to move something on the screen in 2d.
+- Initiate: Vector3 pos = transform.position;
+- Transform: transform.position = pos;
+- Up: pos.y += speed * Time.deltaTime;
+- Down: pos.y -= speed * Time.deltaTime;
+- Left: pos.x -= speed * Time.deltaTime;
+- Right: pos.x += speed * Time.deltaTime;
+*/
---
+++ dvd/Assets/dvd_test_scene/logo_move.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logo_move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public float speed = 3.0f;
+    public float verticalSpeed = 3.0f;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+    	Vector3 pos = transform.position;
+        if(col.gameObject.name == "Left" || col.gameObject.name == "Right")
+        {
+            speed = -speed;
+        }
+        if(col.gameObject.name == "Top" || col.gameObject.name == "Bottom")
+        {
+            verticalSpeed = -verticalSpeed;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;           
+ 		pos.x += speed * Time.deltaTime;
+        pos.y -= verticalSpeed * Time.deltaTime;
+ 		transform.position = pos;
+ 		//Debug.Log(pos);
+         
+    }
+
+
+}
+
+
+/* 
+Here is the stuff you need to move something on the screen in 2d.
+- Initiate: Vector3 pos = transform.position;
+- Transform: transform.position = pos;
+- Up: pos.y += speed * Time.deltaTime;
+- Down: pos.y -= speed * Time.deltaTime;
+- Left: pos.x -= speed * Time.deltaTime;
+- Right: pos.x += speed * Time.deltaTime;
+*/
