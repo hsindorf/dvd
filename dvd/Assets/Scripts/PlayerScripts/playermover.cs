db406da92e93d4d227f8eee8e1f205267dd2b4c4
turnipsoup <jeremy.heckt@gmail.com>
2019-05-28 20:04:00 -0700

    I hope this works

--- dvd/Assets/Scripts/PlayerScripts/playermover.cs
+++ dvd/Assets/Scripts/PlayerScripts/playermover.cs
@@ -11,41 +11,50 @@ public class playermover : MonoBehaviour
     }
 
 
-    void Flip(){
-	    // Switch the way the player is labelled as facing
-	    // Multiply the player's x local scale by -1
-	    Vector3 theScale = transform.localScale;
-	    theScale.x *= -1;
-	    transform.localScale = theScale;
-	}
 
     // Public Variables
     public float speed = 1.2f;
     public bool facingRight;
+    public bool isJumping = false;
+    public int jumpFrames = 0;
+    public float jumpRate = 1.5f;
+    public int jumpLength = 10;
+    
 
     // Update is called once per frame
     void Update()
     {
     	if (Input.GetKey(KeyCode.D)) {
-    		Flip();
-    		facingRight = true;
 
     		Vector3 pos = transform.position;
     		pos.x += speed * Time.deltaTime;
     		transform.position = pos;
     	}
 
-
-
-
     	if (Input.GetKey(KeyCode.A)) {
-    		Flip();
-    		facingRight = false;
 
     		Vector3 pos = transform.position;
     		pos.x -= speed * Time.deltaTime;
     		transform.position = pos;
     	}
+
+        if (Input.GetKey(KeyCode.Space)) {
+            isJumping = true;
+            jumpFrames = jumpLength;
+        }
+
+        if (isJumping && jumpFrames <= jumpLength){
+            Vector3 pos = transform.position;
+            pos.y += speed * jumpRate * Time.deltaTime;
+            transform.position = pos;
+            jumpFrames -= 1;
+
+            if (jumpFrames == 0) {
+                isJumping = false;
+            }
+        }
+            
+
         
     }
 }
