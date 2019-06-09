bbad33d4a8cb0c6f7fb047db573c290e9c43f064
turnipsoup <jeremy.heckt@gmail.com>
2019-06-09 10:14:29 -0700

    added camera tracking

--- dvd/Assets/Scripts/PlayerScripts/playermover.cs
+++ dvd/Assets/Scripts/PlayerScripts/playermover.cs
@@ -13,44 +13,50 @@ public class playermover : MonoBehaviour
 
 
     // Public Variables
-    public float speed = 1.2f;
+    public float speed = 1.6f;
     public bool facingRight;
     public bool isJumping = false;
     public int jumpFrames = 0;
-    public float jumpRate = 1.5f;
+    public float jumpRate = 2.7f;
     public int jumpLength = 10;
+    public float horizontalBoost = 1.8f; // My jumping code (can be fixed later) deals with the speed
+    // variable, so do make the player go faster horizontally I added a boost. Could also be used for collectables
     
 
     // Update is called once per frame
     void Update()
     {
-    	if (Input.GetKey(KeyCode.D)) {
+    	if (Input.GetKey(KeyCode.D)) {                  // Left
 
     		Vector3 pos = transform.position;
-    		pos.x += speed * Time.deltaTime;
+    		pos.x += horizontalBoost * speed * Time.deltaTime;
     		transform.position = pos;
     	}
 
-    	if (Input.GetKey(KeyCode.A)) {
+    	if (Input.GetKey(KeyCode.A)) {                  // Right
 
     		Vector3 pos = transform.position;
-    		pos.x -= speed * Time.deltaTime;
+    		pos.x -= horizontalBoost * speed * Time.deltaTime;
     		transform.position = pos;
     	}
 
-        if (Input.GetKey(KeyCode.Space)) {
-            isJumping = true;
-            jumpFrames = jumpLength;
-        }
+    	if (isJumping != true) {                        // Are you currently jumping?
 
-        if (isJumping && jumpFrames <= jumpLength){
-            Vector3 pos = transform.position;
-            pos.y += speed * jumpRate * Time.deltaTime;
+	    	if (Input.GetKey(KeyCode.Space)) {          // If you are jumping, set the number of jump frames equal
+	            isJumping = true;                       // to the length of the player's current jump
+	            jumpFrames = jumpLength;
+	        }
+    	}
+
+
+        if (isJumping && jumpFrames <= jumpLength){     // Jump Logic that states if the player isjumping is true, and
+            Vector3 pos = transform.position;           // the number of frames left in the jump is less than the player
+            pos.y += speed * jumpRate * Time.deltaTime; // default jump length, then move up and subtract a jump frame
             transform.position = pos;
             jumpFrames -= 1;
 
-            if (jumpFrames == 0) {
-                isJumping = false;
+            if (jumpFrames == 0) {                      // If the number of jump frames left is zero, set isJumping
+                isJumping = false;                      // to false
             }
         }
             
@@ -69,3 +75,5 @@ Here is the stuff you need to move something on the screen in 2d.
 - Left: pos.x -= speed * Time.deltaTime;
 - Right: pos.x += speed * Time.deltaTime;
 */
+
+
